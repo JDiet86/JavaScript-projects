6008169764210e4b926c68b9f307869c566f8e18
JDiet86 <77766468+JDiet86@users.noreply.github.com>
2021-02-02 12:17:33 -0800

    Basic JS Project

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+var A="Welcome!";  //Defining a value and naming it
+window.alert(A);   //Creates alert box
+
+document.write("Joe said to Sally,\"Welcome!\" Sally responded, \"Thank you!\"");   // Utilizing "escaping" to ensure necessary quotations are shown
+
+var B=("\"Fight on my men,\" says Sir Andrew Barton, I am hurt, but I am not slain\;"  // Concatenating a string
++ " I\'ll lay me down and bleed a while, and then I\'ll rise and fight again.\""
++ " -Thomas Moore");
+
+document.write(B);  //Displaying variable "B"
+
+var Teams="NHL", Buffalo="Sabres", Edmonton="Oilers", Boston="Bruins", Detroit="Redwings";  // Defining variables
+var Buffalo=Buffalo.fontcolor("blue");  //Assigning font colors for each variable
+var Edmonton=Edmonton.fontcolor("orange");
+var Boston=Boston.fontcolor('black');
+var Detroit=Detroit.fontcolor('red');
+
+document.write(Buffalo);  //Displaying variable with font color 
+
+
+
+document.write(3+3);  //Displaying expression of 3+3
+
+var X=('65');  // Defining variable 'X'
+document.write(X*2);  //Displaying expression X multiplied by 2
