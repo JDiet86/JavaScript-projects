5692813b9c3cc6894a28247bfd55bc2ae3dc52a8
JDiet86 <77766468+JDiet86@users.noreply.github.com>
2021-02-02 14:53:22 -0800

    Project2 Functions

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function First_Function() {    //Defining a function and naming it
+    var sentence=" JavaScript is really confusing me,";    //Defining a variable and giving it a string value     
+    sentence+=" but I'll keep studying!";                  //Defining a variable and concatenating it with the variable above
+    document.getElementById("Concatenate").innerHTML=sentence;    //Putting the value of result into HTML element with "Concatenate" id
+}
