c0979d72f311aaf6b7ada1728e71444352d28fdf
JDiet86 <77766468+JDiet86@users.noreply.github.com>
2021-02-03 15:37:18 -0800

    Comparisons_Type_Coercion

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,54 @@
+
+function my_Function()  {   //Defining and naming a function
+    document.getElementById("Test").innerHTML=isNaN('543');     //Displays if 543 is a number or not in HTML element "Test"
+}
+
+
+function my_Function1() {    //Defining and naming a function
+    document.getElementById("Test1").innerHTML=isNaN('Words');      //Displays if the string is a number or not in HTML element "Test1"
+}
+
+window.alert(2E310);    //Alert box displaying value as infinity
+
+window.alert(-4E309);   //Alert box displaying value as -infinity
+
+console.log(3*14);      //Displays result of 14 muliplied by 3 in the console log
+
+document.write("15"+ 4);    //Intentionally coercing to display "154"
+
+document.write(8<2);    //Displays whether 8 is less than 2 or not
+
+document.write(2<8);    //Displays whether 2 is less than 8 or not
+
+document.write(typeof "integer");  //Defines "integer" as a string
+
+console.log(15<14);     //Displays result in console log
+
+function Function2() {       //Defining and naming a function
+    document.getElementById("Test2").innerHTML=5==5;    //Displays if 5 is equal to 5 in HTML element "Test2"
+}
+function Function3() {       //Defining and naming a function
+    document.getElementById("Test3").innerHTML=5==4;    //Displays if 5 is equal to 4 in HTML element "Test3"
+}
+
+document.write(5===5);      //Result of comparison of 5 being equal to 5
+document.write(8==="cat");  //Result of comparsion of 8 being equal to "cat"
+document.write(5==="5");    //Result of comparision of 5 being equal to "5"
+document.write(4===5);      //Result of comparison of 4 being equal to 4
+
+console.log(5>2&&10>9);     //Boolean AND logic displayed in console log
+console.log(1==1&&3>4);     //Boolean AND logic displayed in console log
+console.log(5<4||10>11);    //Boolean OR logic displayed in console log
+
+function Function4() {       //Defining and naming a function
+    document.getElementById("Test4").innerHTML=!(5==6);     //Boolean NOT logic displaying if 5 is not equal to 6 in HTML element "Test4"
+}
+
+function Function5() {       //Defining and naming a function
+    document.getElementById("Test5").innerHTML=!(6==6);     //Boolean NOT logic displaying if 6 is not equal to 6 in HTML element "Test5"
+}
+
+
+
+
+ 
